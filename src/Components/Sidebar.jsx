fa63a955f296b6f41483a0d1eb61ff6ef5a49932
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-03-09 13:16:48 +0100

    s

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { SidebarData } from '../utility/datamenu.js'
 import { Link } from 'react-router-dom'
+import '../style/sidebar.css'
 
 
 const Sidebar = () => {
 
     console.log("data", SidebarData)
     return (
-        <div>
-            <div><h1>Logo</h1></div>
+        <div className="Sidebar">
+            <div className='logo'><h1>Logo</h1></div>
             {SidebarData.map((item, index) =>
                 <Link to={item.link} key={index}>
-                    <div>{item.name}{item.icon}</div></Link>
+                    <div className='sidebarList'>
+                        <div className='name'>{item.name}</div><div classname='icon'>{item.icon}</div>
+                    </div>
+                </Link>
             )}
         </div>
     )
