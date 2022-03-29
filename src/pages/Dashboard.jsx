830dae74db5f6c2c2e41755beaa749ffc8c83749
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-03-29 13:09:21 +0200

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,8 +7,15 @@ const Dashboard = () => {
 
     const customers = useSelector((state) => state.customers?.customers.customer)
     console.log("from dashboard", customers)
+    const sortedUscendingByLName = customers?.sort(function (a, b) { return b.name.toUpperCase() - a.name.toUpperCase() })
+    console.log("sorted", sortedUscendingByLName)
 
     const sellers = useSelector((state) => state.sellers?.sellers)
+    console.log("Sellers", sellers)
+
+    const products = useSelector((state) => state.products?.products.product)
+    console.log("amount", products?.amount)
+    console.log("Products", products.sort(function (a, b) { return a.amount - b.amount }))
 
     return (
         <div style={{ color: 'gray' }}>
@@ -16,15 +23,13 @@ const Dashboard = () => {
 
             <div className='d-flex fluid justify-content-around mt-4 flex-wrap'>
                 <div style={{ border: '1px solid gray', height: '30vh', width: '60vh', marginTop: '3%', padding: '3%' }}>
-                    <h5>Best Customer</h5>
-                    <p>more then one product</p>
-                    <p>max total amount</p>
+
+                    <p>Total active customers <span style={{ color: '#1976d2', fontWeight: 'bold' }}> {customers?.length}</span></p>
+                    <p>First 3 best products</p>
                 </div>
-                <div style={{ border: '1px solid gray', height: '30vh', width: '60vh', marginTop: '3%', padding: '3%' }}><h5>Best Seller</h5></div>
-                <div style={{ border: '1px solid gray', height: '30vh', width: '60vh', marginTop: '3%', padding: '3%' }}><h5>Ending products</h5></div>
-                <div style={{ border: '1px solid gray', height: '30vh', width: '60vh', marginTop: '3%', padding: '3%' }}><h5>Ending documents</h5></div>
+
             </div>
-        </div>
+        </div >
     )
 }
 
